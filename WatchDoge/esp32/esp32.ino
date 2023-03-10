97e17c20b92aea96f2aae6b88dcad54ea3eded25
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-03-10 18:53:41 +0700

    Refactor sketches

---
+++ WatchDoge/esp32/esp32.ino
@@ -0,0 +1,23 @@
+/*! 
+ * WatchDoge Arduino Sketch for ESP32 
+ * [ WiFi Gateway Module ]
+ */
+
+// Check if the board is ESP32
+#ifndef ESP32
+#pragma message(THIS SKETCH IS FOR ESP32 ONLY!)
+#error Select ESP32 board.
+#endif
+
+// Include libraries
+#include "conf.h"
+
+// Setup code, run once
+void setup() {
+
+}
+
+// Main code, run repeatedly
+void loop() {
+
+}
